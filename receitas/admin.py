2ae4feced2e9dcd79f73c4428ff95431a3587a10
Israel <israel.usp@gmail.com>
2021-04-24 18:32:10 -0300

    Aula 1.03
    - Atualizando a área admin;

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Receita
 
-admin.site.register(Receita)
+class Listandoreceitas(admin.ModelAdmin):
+    list_display = ('id', 'nomeReceita', 'tempoPreparo')
+    list_display_links = ('id', 'nomeReceita',)
+
+
+admin.site.register(Receita, Listandoreceitas)
