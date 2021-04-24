438f5c5af5dbe0c016b75931afdc23d8c6cb8a3f
Israel <israel.usp@gmail.com>
2021-04-24 18:49:46 -0300

    Aula 2.01
    - Criando um novo app de Pessoas;
    - Criando, migrando e editando um novo modelo;

--- receitas/admin.py
+++ receitas/admin.py
@@ -4,6 +4,9 @@ from .models import Receita
 class Listandoreceitas(admin.ModelAdmin):
     list_display = ('id', 'nomeReceita', 'tempoPreparo')
     list_display_links = ('id', 'nomeReceita',)
+    search_fields = (['nomeReceita'])
+    list_filter = (['categoria'])
+    list_per_page = 15
 
 
 admin.site.register(Receita, Listandoreceitas)
