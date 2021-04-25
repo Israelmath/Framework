67f3d0a0bf7867d3ab08cb6d017341ac37d8adf3
Israel <israel.usp@gmail.com>
2021-04-25 16:40:21 -0300

    Aula 3.02
    - Facilitando alteração de variáveis booleanas no admin;
    - Ordenando SELECT por data de adição;

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import Receita
 
+
 class Listandoreceitas(admin.ModelAdmin):
-    list_display = ('id', 'nomeReceita', 'tempoPreparo')
+    list_display = ('id', 'nomeReceita', 'tempoPreparo', 'publicada')
     list_display_links = ('id', 'nomeReceita',)
     search_fields = (['nomeReceita'])
     list_filter = (['categoria'])
     list_per_page = 15
+
+    list_editable = (['publicada'])
 
 
 admin.site.register(Receita, Listandoreceitas)
--- receitas/views.py
+++ receitas/views.py
@@ -4,7 +4,7 @@ from .models import Receita
 
 def index(request):
 
-    receitas = Receita.objects.filter(publicada=True)
+    receitas = Receita.objects.order_by('-dataCadastro').filter(publicada=True)
 
     dados = {
         'receitas': receitas
