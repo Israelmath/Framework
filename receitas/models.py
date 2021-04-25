bff31a509843b193a81b4147fb9f64c5adc287bf
Israel <israel.usp@gmail.com>
2021-04-25 16:22:09 -0300

    Aula 2.03
    - Melhorando apresentação dos objetos

--- receitas/models.py
+++ receitas/models.py
@@ -13,4 +13,7 @@ class Receita(models.Model):
     categoria = models.CharField(max_length=30)
     dataCadastro = models.DateTimeField(default=datetime.now(), blank=True)
 
+    def __str__(self):
+        return self.nomeReceita
+
 
