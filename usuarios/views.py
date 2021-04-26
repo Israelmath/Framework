0b5d50b8b1d63a85da5e75eea38c45a47575bc9c
Israel <israel.usp@gmail.com>
2021-04-25 21:40:24 -0300

    Aula 3.02
    - Realizando autenticação usando os métodos descritos pela documentação;

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == 'POST':
@@ -35,14 +36,21 @@ def login(request):
     if request.method == 'POST':
         email = request.POST['email']
         senha = request.POST['senha']
-        print(f'\n\nemail: {email} -- senha {senha}')
 
         if email.strip() == '' or senha.strip() == '':
             print('Campos email e senha não podem estar em branco.')
             return redirect('login')
         else:
-            print('Login efetuado com sucesso.')
-            return redirect('dashboard')
+            if User.objects.filter(email=email).exists():
+                nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+                user = auth.authenticate(request, username=nome, password=senha)
+                if user is not None:
+                    auth.login(request, user=user)
+                    print('Login efetuado com sucesso.')
+                    return redirect('dashboard')
+                else:
+                    print('Usuário não cadastrado.')
+                    return redirect('login')
 
     return render(request, 'usuarios/login.html')
 
